989ad9d66762d84baa61b788906a887e2014b497
ariel <santa.cruz.unla@gmail.com>
2023-06-06 23:25:16 -0300

    Agregar archivos de proyecto.

---
+++ Interfaz_IDisposable2/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Interfaz_IDisposable2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_instan = new System.Windows.Forms.Button();
+            this.btn_GC_Collect = new System.Windows.Forms.Button();
+            this.btn_Null = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_instan
+            // 
+            this.btn_instan.Location = new System.Drawing.Point(30, 50);
+            this.btn_instan.Name = "btn_instan";
+            this.btn_instan.Size = new System.Drawing.Size(92, 43);
+            this.btn_instan.TabIndex = 0;
+            this.btn_instan.Text = "instanciar";
+            this.btn_instan.UseVisualStyleBackColor = true;
+            this.btn_instan.Click += new System.EventHandler(this.btn_instan_Click);
+            // 
+            // btn_GC_Collect
+            // 
+            this.btn_GC_Collect.Location = new System.Drawing.Point(269, 50);
+            this.btn_GC_Collect.Name = "btn_GC_Collect";
+            this.btn_GC_Collect.Size = new System.Drawing.Size(127, 43);
+            this.btn_GC_Collect.TabIndex = 1;
+            this.btn_GC_Collect.Text = "G.C. Colletc";
+            this.btn_GC_Collect.UseVisualStyleBackColor = true;
+            this.btn_GC_Collect.Click += new System.EventHandler(this.btn_GC_Collect_Click);
+            // 
+            // btn_Null
+            // 
+            this.btn_Null.Location = new System.Drawing.Point(150, 50);
+            this.btn_Null.Name = "btn_Null";
+            this.btn_Null.Size = new System.Drawing.Size(102, 43);
+            this.btn_Null.TabIndex = 2;
+            this.btn_Null.Text = "nullear";
+            this.btn_Null.UseVisualStyleBackColor = true;
+            this.btn_Null.Click += new System.EventHandler(this.btn_Null_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Null);
+            this.Controls.Add(this.btn_GC_Collect);
+            this.Controls.Add(this.btn_instan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_instan;
+        private System.Windows.Forms.Button btn_GC_Collect;
+        private System.Windows.Forms.Button btn_Null;
+    }
+}
+
---
+++ Interfaz_IDisposable2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaz_IDisposable2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        Documento d;
+
+        private void btn_instan_Click(object sender, EventArgs e)
+        {
+            d = new Documento();
+        }
+        private void btn_Null_Click(object sender, EventArgs e)
+        {
+            //antes de ejecutar el null podemos preguntar si el objeto a nullear contiene un metodo
+            //Dispose()
+            // como las interfaces typan podemos verificar si el documento es del tipo IDisposable
+            // con un if ejecutamo el Dispose() si lo tiene
+            if(d is IDisposable) {d.Dispose();}
+            d = null; // dejo el objeto como basura. Ahora depende del gardbage collector
+        }
+        private void btn_GC_Collect_Click(object sender, EventArgs e)
+        {
+            GC.Collect();
+        }
+    }
+    public class Documento : IDisposable
+        {
+            //una clase que implementa una interfaaz debe implementar sus metodos
+
+            // podemos utilizar un bool para desabilitar el finalizador si usea el Dispose:
+            bool a = true;
+            ~Documento() { if (a) MessageBox.Show("se ejecuto ~Documento (finalizador) "); }
+            //el finalizador se ejecuta cuando termina el ciclo de vida del objeto y pasa el gardbage collector
+            public void Dispose()
+            {
+                MessageBox.Show("Se ejecuto el destructor");
+                //el destructor se ejecuta cuando el programador lo indique
+                //se puede utilizar para soltar recursos no administrados por 
+                //sistema como el consumo de una base de datos
+                a = false;
+            }
+        }
+
+
+    
+}
